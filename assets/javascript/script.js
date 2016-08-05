bd09da6aef647a52f83cda6bda9fc2510b0a5634
Eliot Reyes <eliotmij@gmail.com>
2016-08-05 16:19:10 -0700

    added api functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,15 +1,16 @@
-var searchTerm = 'O.J. Simpson';
+var searchTerm;
 var numOfRecords;
 var startDate;
 var endDate;
 
 var apiKey = 'b9f91d369ff59547cd47b931d8cbc56b:0:74623931';
-var queryURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + searchTerm + '&fields=title%2C+byline%2C+news_desk%2C+date%2C+url&api-key=9d4a8986921972b65754ea0809d47c84%3A12%3A74623931'
+var queryURL;
+var nyresults;
 
 
 function searchArticles(){
   $.ajax({url: queryURL, method: 'GET' }).done(function(response){
-    var nyresults = response.response.docs[0];
+    nyresults = response.response.docs[0];
     console.log(nyresults.headline.main);
 
   });
@@ -20,3 +21,22 @@ function resetFields(){
 }
 
 
+function retrieveValues(){
+  
+  searchTerm = $('#searchTerm').val();
+  numOfRecords = $('#num').val();
+  startDate = $('#startYear').val();
+  endDate = $('#endYear').val();
+
+  queryURL ='http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + searchTerm + '&fields=title%2C+byline%2C+news_desk%2C+date%2C+url&api-key=9d4a8986921972b65754ea0809d47c84%3A12%3A74623931'
+
+}
+
+$('#search').on('click', function(){
+    retrieveValues();
+    searchArticles()
+  });
+
+$(document).ready(function(){
+  
+});
