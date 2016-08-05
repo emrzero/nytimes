fba3ed6575aee653119de3271a1a29b245339f06
Eliot Reyes <eliotmij@gmail.com>
2016-08-05 16:48:07 -0700

    https

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -28,7 +28,7 @@ function retrieveValues(){
   startDate = $('#startYear').val();
   endDate = $('#endYear').val();
 
-  queryURL ='http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + searchTerm + '&api-key=9d4a8986921972b65754ea0809d47c84%3A12%3A74623931';
+  queryURL ='https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + searchTerm + '&api-key=9d4a8986921972b65754ea0809d47c84%3A12%3A74623931';
 
 }
 
