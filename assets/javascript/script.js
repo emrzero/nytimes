e904616bcbede74b1a03c309bda96234687f7127
Eliot Reyes <eliotmij@gmail.com>
2016-08-05 16:26:15 -0700

    fixed code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -28,13 +28,13 @@ function retrieveValues(){
   startDate = $('#startYear').val();
   endDate = $('#endYear').val();
 
-  queryURL ='http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + searchTerm + '&fields=title%2C+byline%2C+news_desk%2C+date%2C+url&api-key=9d4a8986921972b65754ea0809d47c84%3A12%3A74623931'
+  queryURL ='http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + searchTerm + '&api-key=9d4a8986921972b65754ea0809d47c84%3A12%3A74623931';
 
 }
 
 $('#search').on('click', function(){
     retrieveValues();
-    searchArticles()
+    searchArticles();
   });
 
 $(document).ready(function(){
